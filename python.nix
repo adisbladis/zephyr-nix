28395db3a53f0376fee225096b1e52b8e26a8cf8
Otavio Salvador <otavio@ossystems.com.br>
2024-03-01 18:03:44 -0300

    python: Use clang-tools_17 as pypi follows the newest
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- python.nix
+++ python.nix
@@ -1,7 +1,7 @@
 { python3
 , zephyr-src
 , pyproject-nix
-, clang-tools
+, clang-tools_17
 , gitlint
 , lib
 }:
@@ -11,7 +11,7 @@ let
     self = python;
     packageOverrides = self: super: {
       # HACK: Zephyr uses pypi to install non-Python deps
-      clang-format = clang-tools;
+      clang-format = clang-tools_17;
       inherit gitlint;
 
       # HACK: Older Zephyr depends on these missing dependencies
