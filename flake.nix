db825a6ae8b61fe0cd95522b50d6d6f154e78f62
adisbladis <adisbladis@gmail.com>
2024-11-04 03:35:48 +0000

    Use nixpkgs-python fork for compatibility with recent nixpkgs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     pyproject-nix.url = "github:nix-community/pyproject.nix";
     pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+    nixpkgs-python.url = "github:adisbladis/nixpkgs-python/nixpkgs-darwin-sdk-refactor";
     nixpkgs-python.inputs.nixpkgs.follows = "nixpkgs";
 
     zephyr.url = "github:zephyrproject-rtos/zephyr/v3.7.0";
