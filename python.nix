af5ab1506106351d70d7f4437c9b2bae87a979ba
Maximilian Huber <gh@maxhbr.de>
2025-02-02 12:14:31 +0100

    Fix build and be ready for Zephyr 4.0.0
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>
    Modified-by: adisbladis <adisbladis@gmail.com>

--- python.nix
+++ python.nix
@@ -42,6 +42,35 @@ let
 
       # HACK: Older Zephyr depends on these missing dependencies
       sphinxcontrib-svg2pdfconverter = super.sphinxcontrib-svg2pdfconverter or null;
+
+      # Prefer existing sphinx-lint, otherwise fall back to manual packaging.
+      sphinx-lint = super.sphinx-lint or (self.buildPythonPackage {
+        pname = "sphinx-lint";
+        version = "1.0.0";
+        format = "wheel";
+
+        src = pkgs.fetchurl {
+          url = "https://files.pythonhosted.org/packages/31/d2/a130ffba531af7cbbb0e7ad24c7d577d3de0b797437f61d3a7234ed6d836/sphinx_lint-1.0.0-py3-none-any.whl";
+          hash = "sha256-YReg80Cy3HPq38V9t1MdRHfgkp+SoMGi9h5u28Jy8Lw=";
+        };
+
+        propagatedBuildInputs = [
+          self.polib
+          self.regex
+        ];
+      });
+
+      # Prefer existing vermin, otherwise fall back to manual packaging.
+      vermin = super.vermin or (self.buildPythonPackage {
+        pname = "vermin";
+        version = "1.6.0";
+        format = "wheel";
+
+        src = pkgs.fetchurl {
+          url = "https://files.pythonhosted.org/packages/2e/98/1a2ca43e6d646421eea16ec19977e2e6d1ea9079bd9d873bfae513d43f1c/vermin-1.6.0-py2.py3-none-any.whl";
+          hash = "sha256-8fqe5A9ZmD3EDgR36ysfqAYaPfTDsrzzSa3UYqVhDvs=";
+        };
+      });
     };
   };
 
